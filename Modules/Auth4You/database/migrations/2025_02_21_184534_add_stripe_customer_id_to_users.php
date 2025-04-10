6a1ce4405b1f3accb183b9f39e0e2a232930745d
marti david tendilla gonzalez <martidavid.tendilla@gmail.com>
2025-04-09 18:57:48 -0600

    se agregan modulo de excel

--- Modules/Auth4You/database/migrations/2025_02_21_184534_add_stripe_customer_id_to_users.php
+++ Modules/Auth4You/database/migrations/2025_02_21_184534_add_stripe_customer_id_to_users.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::connection('auth_db')->table('users', function (Blueprint $table) {
             $table->string('stripe_customer_id')->nullable()->after('password');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::connection('auth_db')->table('users', function (Blueprint $table) {
             $table->dropColumn('stripe_customer_id');
         });
     }
---
+++ Modules/ExcelCore/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('excelcore::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('excelcore.name') !!}</p>
+@endsection
